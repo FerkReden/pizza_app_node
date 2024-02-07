9da0dcf6a1ebad35eb9ff213b76236d3b8ae968b
FerkReden <depper731@gmail.com>
2024-02-07 17:57:30 +0200

    Add token

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 const pizzaRouter = require('./routers/pizza');
 const userRouter = require('./routers/user');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,8 +10,5 @@ app.use(pizzaRouter);
 app.use(userRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  
+  console.log(`Server is running on port ${port}`); 
 });
-
-
--- src/routers/pizza.ts
+++ src/routers/pizza.ts
@@ -1,25 +1,8 @@
 import express from 'express';
-import { Request, Response } from 'express';
-import db from '../db/mysql';
+const pizzaService = require('../services/pizza-service');
 
 const router = express.Router();
 
-router.get('/pizza_menu', async (req: Request, res: Response) => {
-    try {
-        const results = await new Promise<any>((resolve, rejects) => {
-            db.query('SELECT * FROM pizza_menu', (err, results) => {
-                if (err) {
-                    rejects(err);
-                    return;
-                }
-                resolve(results)
-            });
-        });
-
-        res.json(results)
-    } catch (err: any) {
-        res.status(500).json({ error: err.message})
-    }
-});
+router.get('/pizza_menu', pizzaService.getMenu);
 
 module.exports = router
